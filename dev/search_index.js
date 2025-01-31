978804197f4814b0493fb6f98800bf69986326f5
Documenter.jl <documenter@juliadocs.github.io>
2025-01-31 14:05:53 +0000

    build based on 4c2f0cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MOPED","category":"page"},{"location":"#MOPED","page":"Home","title":"MOPED","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MOPED.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MOPED]","category":"page"}]
+}
